e4474744c8a9fee23c6b5df6ffd2d45c8fe861eb
kcolcy <143050750+kcolcy@users.noreply.github.com>
2026-06-15 19:15:43 +0800

    添加编译器路径说明
    Refactor keil2cmake.py to improve structure and readability.

--- keil2cmake.py
+++ keil2cmake.py
@@ -232,7 +232,7 @@ set(CMAKE_SYSTEM_PROCESSOR          arm)
 set(CMAKE_C_COMPILER_ID ARMClang)
 set(CMAKE_CXX_COMPILER_ID ARMClang)
 
-# ARMCLANG V6 from Keil MDK C/C++ compiler
+# ARMCLANG V6 from Keil MDK C/C++ compiler 用户需要自行调整实际路径
 set(TOOLCHAIN_PATH                "D:\\\\Software\\\\Keil_v5\\\\ARM\\\\ARMCLANG\\\\bin\\\\")
 
 set(CMAKE_C_COMPILER                "${TOOLCHAIN_PATH}armclang.exe")
